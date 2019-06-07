e84bef80d438e621e6c6e2b9c73b0c220526cd01
Ali Sardari <ali.sardari@yahoo.com>
2019-06-07 23:59:03 +0430

    - Add Button

--- widget/src/main/java/sardari/widget/Button.java
+++ widget/src/main/java/sardari/widget/Button.java
@@ -9,33 +9,28 @@ import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
 import android.graphics.drawable.RippleDrawable;
-import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.StateListDrawable;
-import android.graphics.drawable.shapes.RoundRectShape;
 import android.os.Build;
+import android.os.Handler;
 import android.os.SystemClock;
 import android.support.annotation.Nullable;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.AppCompatButton;
 import android.util.AttributeSet;
-import android.util.TypedValue;
+import android.util.Log;
 import android.view.View;
 
-import java.util.Arrays;
-
 public class Button extends AppCompatButton {
+    private Handler handler;
     private static int defaultClickDelay = 1000; //ms
     private boolean isDisableDoubleClick = true;
     private int clickDelay = defaultClickDelay;
     private long lastClickTime = 0;
 
     private Integer backgroundColor;
+    private Integer disableColor;
     private Integer borderColor;
     private Integer rippleColor;
     private float cornerRadius;
-
-    private Drawable foreground;
-    private Drawable background;
 
     private boolean hasRipple = false;
 
@@ -56,24 +51,30 @@ public class Button extends AppCompatButton {
     }
 
     private void initViews(Context context, AttributeSet attrs) {
+        handler = new Handler(context.getMainLooper());
+
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.Button, 0, 0);
 
         try {
             clickDelay = typedArray.getInteger(R.styleable.Button_clickDelay, defaultClickDelay);
             isDisableDoubleClick = typedArray.getBoolean(R.styleable.Button_disableDoubleClick, true);
-            rippleColor = typedArray.getColor(R.styleable.Button_rippleColor, 0);
-            cornerRadius = typedArray.getDimension(R.styleable.Button_cornerRadius, context.getResources().getColor(R.color.defaultRippleColor));
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                if (getBackground() instanceof RippleDrawable) {
-                    hasRipple = true;
-                }
+            rippleColor = typedArray.getColor(R.styleable.Button_rippleColor, context.getResources().getColor(R.color.defaultRippleColor));
+            cornerRadius = typedArray.getDimension(R.styleable.Button_cornerRadius, context.getResources().getDimension(R.dimen.corner_radius));
+            if (typedArray.hasValue(R.styleable.Button_disableColor)) {
+                disableColor = typedArray.getColor(R.styleable.Button_disableColor, context.getResources().getColor(R.color.disableColor));
             }
 
+            if (getBackground() instanceof StateListDrawable) {
+                hasRipple = true;
+            }
+
+            Log.w("MyTag", " Background-> " + getBackground());
+            Log.w("MyTag", " hasRipple-> " + hasRipple);
+
             //region Background
-            if (typedArray.hasValue(R.styleable.Button_backgroundColor)) {
-                backgroundColor = typedArray.getColor(R.styleable.Button_backgroundColor, -1);
-            } else if (!hasRipple) {
+            if (typedArray.hasValue(R.styleable.Button_defaultColor)) {
+                backgroundColor = typedArray.getColor(R.styleable.Button_defaultColor, -1);
+            } else {
                 if (getBackground() instanceof ColorDrawable) {
                     backgroundColor = ((ColorDrawable) getBackground()).getColor();
                 }
@@ -82,30 +83,15 @@ public class Button extends AppCompatButton {
 
             //region Border
             if (typedArray.hasValue(R.styleable.Button_borderColor)) {
-                borderColor = typedArray.getColor(R.styleable.Button_borderColor, backgroundColor);
-            } else if (!hasRipple) {
-                borderColor = backgroundColor;
+                borderColor = typedArray.getColor(R.styleable.Button_borderColor, context.getResources().getColor(android.R.color.transparent));
             }
             //endregion
 
-            if (backgroundColor != null) {
-                background = drawRoundRect(backgroundColor, borderColor);
-                setBackground(background);
-            }
-
-            //region Foreground
-            if (!hasRipple || backgroundColor != null) {
-                foreground = typedArray.getDrawable(R.styleable.Button_foreground);
-                if (foreground != null) {
-                    setForeground(foreground);
-                } else {
-                    TypedValue outValue = new TypedValue();
-                    getContext().getTheme().resolveAttribute(R.attr.selectableItemBackgroundBorderless, outValue, true);
-                    Drawable _foreground = ContextCompat.getDrawable(context, outValue.resourceId);
-                    setForeground(_foreground);
+            if (!hasRipple) {
+                if (backgroundColor != null) {
+                    setBackground(generateRippleDrawable());
                 }
             }
-            //endregion
         } finally {
             typedArray.recycle();
         }
@@ -115,6 +101,14 @@ public class Button extends AppCompatButton {
     @Override
     public boolean performClick() {
         if (isDisableDoubleClick) {
+            setEnabled(false);
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    setEnabled(true);
+                }
+            }, clickDelay);
+
             //region Preventing multiple clicks, using threshold of 1 second
             if (SystemClock.elapsedRealtime() - lastClickTime < clickDelay) {
                 return false;
@@ -131,6 +125,11 @@ public class Button extends AppCompatButton {
     @Override
     public void setOnClickListener(@Nullable View.OnClickListener l) {
         super.setOnClickListener(l);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
     }
 
     //------------------------------------------------------------------------------------
@@ -187,6 +186,18 @@ public class Button extends AppCompatButton {
     //endregion
 
     //region RippleColor
+    public Integer getRippleAlphaColor() {
+        if (rippleColor != null) {
+            int red = Color.red(rippleColor);
+            int green = Color.green(rippleColor);
+            int blue = Color.blue(rippleColor);
+
+            return Color.argb(80, red, green, blue);
+        } else {
+            return null;
+        }
+    }
+
     public Integer getRippleColor() {
         return rippleColor;
     }
@@ -194,149 +205,74 @@ public class Button extends AppCompatButton {
     public void setRippleColor(Integer rippleColor) {
         this.rippleColor = rippleColor;
     }
-    //    //endregion
+    //endregion
 
-    //------------------------------------------------------------------------------------
-    //region Foreground
-
-    public void setForegroundResource(int drawableResId) {
-        setForeground(ContextCompat.getDrawable(getContext(), drawableResId));
+    //region DisableColor
+    public Integer getDisableColor() {
+        return disableColor;
     }
 
-    public void setForeground(Drawable drawable) {
-        if (foreground == drawable) {
-            return;
-        }
-
-        if (foreground != null) {
-            foreground.setCallback(null);
-            unscheduleDrawable(foreground);
-        }
-
-        drawable = getAdaptiveRippleDrawable(getRippleColor());
-        foreground = drawable;
-
-        if (drawable != null) {
-            drawable.setCallback(this);
-            if (drawable.isStateful()) {
-                drawable.setState(getDrawableState());
-            }
-        }
-
-        requestLayout();
-        invalidate();
-    }
-
-    @Override
-    protected boolean verifyDrawable(Drawable who) {
-        return super.verifyDrawable(who) || who == foreground;
-    }
-
-    @Override
-    public void jumpDrawablesToCurrentState() {
-        super.jumpDrawablesToCurrentState();
-        if (foreground != null) foreground.jumpToCurrentState();
-    }
-
-    @Override
-    protected void drawableStateChanged() {
-        super.drawableStateChanged();
-        if (foreground != null && foreground.isStateful()) {
-            foreground.setState(getDrawableState());
-        }
-    }
-
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        if (foreground != null) {
-            foreground.setBounds(0, 0, getMeasuredWidth(), getMeasuredHeight());
-            invalidate();
-        }
-    }
-
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-        if (foreground != null) {
-            foreground.setBounds(0, 0, w, h);
-            invalidate();
-        }
-    }
-
-    @Override
-    public void draw(Canvas canvas) {
-        super.draw(canvas);
-
-        if (foreground != null) {
-            foreground.draw(canvas);
-        }
+    public void setDisableColor(Integer disableColor) {
+        this.disableColor = disableColor;
     }
     //endregion
 
     //------------------------------------------------------------------------------------
-    private GradientDrawable drawRoundRect(Integer backgroundColor, Integer borderColor) {
-        GradientDrawable shape = new GradientDrawable();
-        shape.setShape(GradientDrawable.RECTANGLE);
 
-        if (backgroundColor != null) {
-            shape.setColor(backgroundColor);
-        }
-
-        if (borderColor != null) {
-            shape.setStroke(1, borderColor);
-        }
-
-        shape.setCornerRadius(getCornerRadius());
-
-        return shape;
-    }
-
-    public Drawable getAdaptiveRippleDrawable(int color) {
-        int red = Color.red(color);
-        int green = Color.green(color);
-        int blue = Color.blue(color);
-
-        int rippleColor;
-
+    private Drawable generateRippleDrawable() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            rippleColor = Color.argb(100, red, green, blue);
-            return new RippleDrawable(ColorStateList.valueOf(rippleColor), null, getRippleMask());
-//            return new RippleDrawable(ColorStateList.valueOf(rippleColor), null, getRippleMask(rippleColor));
+            if (getRippleColor() != null) {
+                return new RippleDrawable(ColorStateList.valueOf(getRippleAlphaColor()), generateGradientDrawable(), null);
+            } else {
+                return null;
+            }
         } else {
-            rippleColor = Color.argb(80, red, green, blue);
-            return getStateListDrawable(rippleColor);
+            return generateStateListDrawable();
         }
     }
 
-    private Drawable getRippleMask() {
-        float[] outerRadii = new float[8];
-        Arrays.fill(outerRadii, getCornerRadius());
-
-        RoundRectShape r = new RoundRectShape(outerRadii, null, null);
-        return new ShapeDrawable(r);
-    }
-
-    private Drawable getRippleMask(int color) {
-        float[] outerRadii = new float[8];
-        Arrays.fill(outerRadii, getCornerRadius());
-
-        RoundRectShape r = new RoundRectShape(outerRadii, null, null);
-        ShapeDrawable shapeDrawable = new ShapeDrawable(r);
-        shapeDrawable.getPaint().setColor(color);
-        return shapeDrawable;
-    }
-
-    public StateListDrawable getStateListDrawable(int pressedColor) {
+    private StateListDrawable generateStateListDrawable() {
         StateListDrawable states = new StateListDrawable();
-        GradientDrawable gradientDrawable = new GradientDrawable();
-        gradientDrawable.setCornerRadius(getCornerRadius());
-        gradientDrawable.setColor(pressedColor);
 
-        states.addState(new int[]{android.R.attr.state_pressed}, gradientDrawable);
-        states.addState(new int[]{android.R.attr.state_focused}, gradientDrawable);
-        states.addState(new int[]{android.R.attr.state_activated}, gradientDrawable);
+        //region defaultGradientDrawable
+        GradientDrawable defaultGradientDrawable = new GradientDrawable();
+        defaultGradientDrawable.setCornerRadius(getCornerRadius());
+        if (getBackgroundColor() != null) {
+            defaultGradientDrawable.setColor(getBackgroundColor());
+        }
+        if (getBorderColor() != null) {
+            defaultGradientDrawable.setStroke(1, getBorderColor());
+        }
+        //endregion
+
+        //region rippleGradientDrawable
+        GradientDrawable rippleGradientDrawable = new GradientDrawable();
+        rippleGradientDrawable.setCornerRadius(getCornerRadius());
+        if (getRippleColor() != null) {
+            rippleGradientDrawable.setColor(getRippleColor());
+        }
+        if (getBorderColor() != null) {
+            rippleGradientDrawable.setStroke(1, getBorderColor());
+        }
+        //endregion
+
+        states.addState(new int[]{android.R.attr.state_pressed}, rippleGradientDrawable);
+        states.addState(new int[]{android.R.attr.state_focused}, rippleGradientDrawable);
+        states.addState(new int[]{android.R.attr.state_activated}, rippleGradientDrawable);
+        states.addState(new int[]{}, defaultGradientDrawable);
         return states;
     }
-    //------------------------------------------------------------------------------------
+
+    private GradientDrawable generateGradientDrawable() {
+        GradientDrawable defaultGradientDrawable = new GradientDrawable();
+        defaultGradientDrawable.setCornerRadius(getCornerRadius());
+        if (getBackgroundColor() != null) {
+            defaultGradientDrawable.setColor(getBackgroundColor());
+        }
+        if (getBorderColor() != null) {
+            defaultGradientDrawable.setStroke(1, getBorderColor());
+        }
+
+        return defaultGradientDrawable;
+    }
 }
